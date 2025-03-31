937c981a75cd6157d00715c58e265c36b19f5a42
MinhKhoiMap <helloiamkhoi@gmail.com>
2025-03-31 20:05:57 +0700

    add try catch

--- src/app/[locale]/(mainpage)/news/page.tsx
+++ src/app/[locale]/(mainpage)/news/page.tsx
@@ -10,32 +10,36 @@ export default async function NewsPage({ searchParams, params }: RequestProps) {
   const locale = (await params).locale;
   const page = (await searchParams).page;
 
-  const {
-    payload: { data, totalPage },
-  } = await getPostServices.getList("news", page, locale);
-
-  return (
-    <>
-      <section className="section">
-        <div className="container">
-          <div className="row">
-            {data.map(({ title, thumbnail, publishDate, id }) => (
-              <div className="col-lg-4 col-sm-6 mb-4" key={id}>
-                <DateCard
-                  basePath="news"
-                  title={title}
-                  thumb={thumbnail}
-                  publishDate={publishDate}
-                  id={id}
-                />
-              </div>
-            ))}
+  try {
+    const {
+      payload: { data, totalPage },
+    } = await getPostServices.getList("news", page, locale);
+  
+    return (
+      <>
+        <section className="section">
+          <div className="container">
+            <div className="row">
+              {data.map(({ title, thumbnail, publishDate, id }) => (
+                <div className="col-lg-4 col-sm-6 mb-4" key={id}>
+                  <DateCard
+                    basePath="news"
+                    title={title}
+                    thumb={thumbnail}
+                    publishDate={publishDate}
+                    id={id}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      </section>
-      <Pagination totalPage={totalPage} />
-      {/* useContext has useable */}
-      <PageHeaderContextComponent title={"news"} description={""} />
-    </>
-  );
+        </section>
+        <Pagination totalPage={totalPage} />
+        {/* useContext has useable */}
+        <PageHeaderContextComponent title={"news"} description={""} />
+      </>
+    );
+  } catch (error) {
+    console.log(error)
+  }
 }
